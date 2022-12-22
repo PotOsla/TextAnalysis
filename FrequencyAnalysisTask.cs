e7d6e0373d0abbf4dca6f3d1a279d8c1268c212d
Mikhail <smirskijj@gmail.com>
2022-12-22 12:27:28 +0300

    Добавьте файлы проекта.

---
+++ FrequencyAnalysisTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace TextAnalysis
+{
+    static class FrequencyAnalysisTask
+    {
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+        {
+            var result = new Dictionary<string, string>();
+            var counter = new Dictionary<string, int>();
+            foreach (var sentence in text)
+            {
+                foreach (var word in sentence)
+                {
+                    var index = sentence.IndexOf(word);
+                    if (index + 1 < sentence.Count)
+                    {
+                        var doubleWord = word + "," + sentence[index + 1];
+                        if (counter.ContainsKey(doubleWord))
+                        {
+                            counter[doubleWord]++;
+                        }
+                        else
+                        {
+                            counter[doubleWord] = 1;
+                        }
+                        if (index + 2 < sentence.Count)
+                        {
+                            var tripleWord = doubleWord + "," + sentence[index + 2];
+                            if (!counter.ContainsKey(tripleWord)) counter[tripleWord] = 0;
+                            counter[tripleWord]++;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+   }
+}
---
+++ SentencesParserTask.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        private static List<string> GetWords(string sentence)
+        {
+            var temp = new List<string>();
+            string tempword = string.Empty;
+            foreach (var bukva in sentence)
+            {
+                if (char.IsLetter(bukva) || bukva == '\'')
+                {
+                    tempword += char.ToLower(bukva);
+                }
+                else if (!string.IsNullOrEmpty(tempword))
+                {
+                    temp.Add(tempword);
+                    tempword = string.Empty;
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(tempword))
+            {
+                temp.Add(tempword);
+            }
+            return temp;
+        }
+
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var stringList = text.Split(new char[] { '.', '!', '?', ':', ';', '(', ')' });
+            foreach (var sentence in stringList)
+            {
+                if (string.IsNullOrWhiteSpace(sentence))
+                {
+                    continue;
+                }
+                var temp = SentencesParserTask.GetWords(sentence);
+                if (temp.Count > 0)
+                {
+                    sentencesList.Add(temp);
+                }
+            }
+            return sentencesList;
+        }
+    }
+}
