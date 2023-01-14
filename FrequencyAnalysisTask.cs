ccf0370680d66d9b48758acf792e35635feee1fc
Mikhail <smirskijj@gmail.com>
2023-01-14 12:25:24 +0300

    finished project

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -10,33 +10,67 @@ namespace TextAnalysis
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
             var result = new Dictionary<string, string>();
-            var counter = new Dictionary<string, int>();
+            var counter = new Dictionary<string, Dictionary<string, int>>();
             foreach (var sentence in text)
             {
-                foreach (var word in sentence)
+                for (var index = 0; index < sentence.Count - 1; index++)
                 {
-                    var index = sentence.IndexOf(word);
-                    if (index + 1 < sentence.Count)
+                    var second = sentence[index + 1];
+                    var word = sentence[index];
+                    AddGramm(counter, word, second);
+                    if (index < sentence.Count - 2)
                     {
-                        var doubleWord = word + "," + sentence[index + 1];
-                        if (counter.ContainsKey(doubleWord))
-                        {
-                            counter[doubleWord]++;
-                        }
-                        else
-                        {
-                            counter[doubleWord] = 1;
-                        }
-                        if (index + 2 < sentence.Count)
-                        {
-                            var tripleWord = doubleWord + "," + sentence[index + 2];
-                            if (!counter.ContainsKey(tripleWord)) counter[tripleWord] = 0;
-                            counter[tripleWord]++;
-                        }
+                        var doubleWord = $"{word} {second}";
+                        var third = sentence[index + 2];
+                        AddGramm(counter, doubleWord, third);
                     }
                 }
+
+            }
+            foreach (var i in counter)
+            {
+                var alpha = i.Key;
+                var omega = i.Value;
+                int maxtemp = 0;
+                foreach (var n in omega)
+                {
+                    if (n.Value > maxtemp)
+                    {
+                        maxtemp = n.Value;
+                    }
+                }
+                var tempword = "";
+                foreach (var word in omega)
+                {
+                    if (word.Value == maxtemp)
+                    {
+                        if (!string.IsNullOrEmpty(tempword))
+                        {
+                            int compare = string.CompareOrdinal(tempword, word.Key);
+                            if (compare > 0)
+                            {
+                                tempword = word.Key;
+                            }
+                        }
+                        else tempword = word.Key;
+                    }
+                }
+                result.Add(alpha, tempword);
             }
             return result;
         }
-   }
+
+        private static void AddGramm(Dictionary<string, Dictionary<string, int>> counter, string word, string second)
+        {
+            if (!counter.ContainsKey(word))
+            {
+                counter.Add(word, new Dictionary<string, int>());
+            }
+            if (!counter[word].ContainsKey(second))
+            {
+                counter[word].Add(second, 0);
+            }
+            counter[word][second]++;
+        }
+    }
 }
--- TextGeneratorTask.cs
+++ TextGeneratorTask.cs
@@ -9,6 +9,29 @@ namespace TextAnalysis
             string phraseBeginning,
             int wordsCount)
         {
+            string[] phrasewords;
+            for (int i = 0; i < wordsCount; i++)
+            {
+                phrasewords = phraseBeginning.Split(new char[] { ' ' });
+                string twoLastWords = "";
+                string oneLastWords = phrasewords[phrasewords.Length - 1];
+                if (phrasewords.Length >= 2)
+                {
+                    twoLastWords = $"{phrasewords[phrasewords.Length - 2]} {phrasewords[phrasewords.Length - 1]}";
+                }
+                string temp = "";
+                if (nextWords.ContainsKey(twoLastWords))
+                {
+                    temp = twoLastWords;
+                }
+                else if (nextWords.ContainsKey(oneLastWords))
+                {
+                    temp = oneLastWords;
+                }
+                else break;
+                temp = nextWords[temp];
+                phraseBeginning = phraseBeginning + " " + temp;
+            }
             return phraseBeginning;
         }
     }
